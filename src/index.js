8645070f04404731c09061535d843fe0745b3f86
Konstantyn <k.buhantsev@gmail.com>
2022-10-06 11:34:45 +0300

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-02-feedback/">
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter basename="/goit-react-hw-02-feedback/"> */}
+    <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
